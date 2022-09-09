d9c10b6ae70b23d3d65e60c989c5eafe112e9c7f
Ashley <aforrest56@gmail.com>
2022-09-08 22:39:33 -0500

    still trying

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -128,15 +128,16 @@ public class TechJobs {
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
         if( someJobs.size() >= 1){
             for(HashMap<String, String> i : someJobs){
-                System.out.println("*****\n" +
-                        "position type: " + i.get("position type") +
+                System.out.println("\n*****" +
+                        "\nposition type: " + i.get("position type") +
                         "\nname: " + i.get("name") +
                         "\nemployer: " + i.get("employer") +
                         "\nlocation: " + i.get("location") +
                         "\ncore competency: " + i.get("core competency") +
-                        "\n*****\n");
+                        "\n*****");
             }
-        }else { System.out.println("No Results");
+        }else {
+            System.out.println("No Results");
         }
 
     }
